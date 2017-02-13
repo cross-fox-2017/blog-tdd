f59cf569a79224065342e81b2dd9bbe96e0734a2
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-02-13 17:00:41 +0700

    add blog route

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,16 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/users')
 
+// User Routes
 router.get('/', controller.path)
 router.post('/users', controller.addUsers)
 router.get('/users', controller.getAllUsers)
 router.delete('/users/:id', controller.deleteUser)
+
+// Blog Routes
+router.post('/blogs', controller.addBlog)
+router.get('/blogs', controller.getAllBlogs)
+router.delete('/blogs/:id', controller.deleteBlog)
+router.put('/blogs/:id', controller.updateBlog)
 
 module.exports = router
