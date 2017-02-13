0cf411a6b7407878965bd13fbbed50d41a7d7e9b
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-13 17:22:41 +0700

    route

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,11 @@
 var express = require('express')
 var router = express.Router()
-var MODEL = require('../models/article')
+var controllers = require('../controllers/article')
 
 /* GET home page. */
 
-router.post('/article', function (req, res) {
-  let article = {
-    author: req.body.author,
-    title: req.body.title,
-    body: req.body.body,
-    is_deleted: 0
-  }
-  MODEL.create(article)
-    .then(function (data) {
-      res.send(data)
-    }).catch(function (err) {
-    res.send(err)
-  })
-})
-
-router.get('/articles', function (req, res) {
-  MODEL.find({is_deleted: 0})
-    .then(function (data) {
-      res.send(data)
-    }).catch(function (err) {
-    res.send(err)
-  })
-})
-
-router.delete('/article', function (req, res) {
-  let query = {
-    title: req.body.title
-  }
-  MODEL.find(query)
-    .then(function (data) {
-      data[0].is_deleted = 1
-      data[0].save(function (err) {
-        if (err)
-          console.log(err)
-      })
-      res.send('Successfully Deleted')
-    }).catch(function (err) {
-    res.send(err)
-  })
-})
+router.post('/article', controllers.newArticle)
+router.get('/articles', controllers.getAllArticle)
+router.delete('/article', controllers.deleteArticle)
 
 module.exports = router
