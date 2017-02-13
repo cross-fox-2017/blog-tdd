a8749011382a6289e01e3db0b166465714bdb39c
Alexander Hendrawan tjahja <alexanderhendrawan@gmail.com>
2017-02-13 15:07:30 +0700

    create client and server folder

---
+++ server/controllers/controller.users.js
@@ -0,0 +1,11 @@
+const ../models/model.users = require('../models/model.users');
+
+var controllerUser = {
+  getAllUser: function (req, res) {},
+  getOneUser: function (req, res) {},
+  createOneUser: function (req, res) {},
+  editOneUser: function (req, res) {},
+  deleteOneUser: function (req, res) {}
+}
+
+module.exports = controllerUser
