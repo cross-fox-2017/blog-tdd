dc06a7c0b2b2d14b46e361f1c4a166e559075759
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-02-13 17:22:32 +0700

    app

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', routes)
-app.use('/users', users)
+app.use('/API', users)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
